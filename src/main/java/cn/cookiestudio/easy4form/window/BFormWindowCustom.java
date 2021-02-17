52c3967186ee8ecbdbbbd9af9ad9c4ebcda92dc5
smart_cmd <3523206925@qq.com>
2021-02-17 22:15:24 +0800

    az

--- src/main/java/cn/cookiestudio/easy4form/window/BFormWindowCustom.java
+++ src/main/java/cn/cookiestudio/easy4form/window/BFormWindowCustom.java
@@ -1,5 +1,63 @@
 package cn.cookiestudio.easy4form.window;
 
-public class BFormWindowCustom {
+import cn.cookiestudio.easy4form.PluginMain;
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.player.PlayerFormRespondedEvent;
+import cn.nukkit.form.element.Element;
+import cn.nukkit.form.element.ElementButtonImageData;
+import cn.nukkit.form.response.FormResponseCustom;
+import cn.nukkit.form.window.FormWindowCustom;
+import java.util.List;
+import java.util.function.Function;
 
+public class BFormWindowCustom extends FormWindowCustom {
+
+    private int formId;
+    private Function<FormResponseCustom, Void> onResponse;
+
+    public BFormWindowCustom(String title) {
+        super(title);
+    }
+
+    public BFormWindowCustom(String title, List<Element> contents) {
+        super(title, contents);
+    }
+
+    public BFormWindowCustom(String title, List<Element> contents, String icon) {
+        super(title, contents, icon);
+    }
+
+    public BFormWindowCustom(String title, List<Element> contents, ElementButtonImageData icon) {
+        super(title, contents, icon);
+    }
+
+    public BFormWindowCustom(String title, List<Element> contents, ElementButtonImageData icon, Function<FormResponseCustom, Void> onResponse){
+        super(title, contents, icon);
+        this.onResponse = onResponse;
+    }
+
+    public Function<FormResponseCustom, Void> getResponseAction() {
+        return onResponse;
+    }
+
+    public void invokeAction(FormResponseCustom response){
+        this.onResponse.apply(response);
+    }
+
+    public int sendToPlayer(Player player){
+        Server.getInstance().getPluginManager().registerEvents(new BFormWindowCustom.Listener(), PluginMain.getPluginMain());
+        this.formId = player.showFormWindow(this);
+        return this.formId;
+    }
+
+    private class Listener implements cn.nukkit.event.Listener{
+        @EventHandler
+        public void onFormResponse(PlayerFormRespondedEvent event){
+            if (event.getFormID() == BFormWindowCustom.this.formId){
+                ((BFormWindowCustom)event.getWindow()).invokeAction((FormResponseCustom)event.getResponse());
+            }
+        }
+    }
 }
--- src/main/java/cn/cookiestudio/easy4form/window/BFormWindowModal.java
+++ src/main/java/cn/cookiestudio/easy4form/window/BFormWindowModal.java
@@ -1,4 +1,54 @@
 package cn.cookiestudio.easy4form.window;
 
-public class BFormWindowModal {
+import cn.cookiestudio.easy4form.PluginMain;
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.player.PlayerFormRespondedEvent;
+import cn.nukkit.form.response.FormResponseModal;
+import cn.nukkit.form.window.FormWindowModal;
+
+import java.util.function.Function;
+
+public class BFormWindowModal extends FormWindowModal {
+
+    private int formId;
+    private Function<FormResponseModal, Void> onResponse;
+
+    public BFormWindowModal(String title, String content, String trueButtonText, String falseButtonText) {
+        super(title, content, trueButtonText, falseButtonText);
+    }
+
+    public BFormWindowModal(String title, String content, String trueButtonText, String falseButtonText, Function<FormResponseModal, Void> onResponse){
+        super(title, content, trueButtonText, falseButtonText);
+        this.onResponse = onResponse;
+    }
+
+    public BFormWindowModal setResponseAction(Function<FormResponseModal, Void> onResponse){
+        this.onResponse = onResponse;
+        return this;
+    }
+
+    public Function<FormResponseModal, Void> getResponseAction() {
+        return onResponse;
+    }
+
+    public void invokeAction(FormResponseModal response){
+        this.onResponse.apply(response);
+    }
+
+    public int sendToPlayer(Player player){
+        Server.getInstance().getPluginManager().registerEvents(new BFormWindowModal.Listener(), PluginMain.getPluginMain());
+        this.formId = player.showFormWindow(this);
+        return this.formId;
+    }
+
+    private class Listener implements cn.nukkit.event.Listener{
+        @EventHandler
+        public void onFormResponse(PlayerFormRespondedEvent event){
+            if (event.getFormID() == BFormWindowModal.this.formId){
+                ((BFormWindowModal)event.getWindow()).invokeAction((FormResponseModal)event.getResponse());
+            }
+        }
+    }
 }
